c938ecaa49582c93450bf4085401a33697b1c14e
BlockSC <79803546+BlockSC@users.noreply.github.com>
2023-09-11 16:27:06 +0200

    update

--- car-rental-common/Classes/Customer.cs
+++ car-rental-common/Classes/Customer.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace car_rental_common.Classes
 {
-    internal class Customer
+    public class Customer
     {
+        private string _sSN;
+        public string SSN
+        {
+            get { return _sSN; }
+            set
+            {
+                if (IsValidSSN(value))
+                {
+                    _sSN = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Social security number must be in the format 123456.");
+                }
+            }
+        }
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                if (IsValidName(value))
+                {
+                    _firstName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Names can only contain letters.");
+                }
+            }
+        }
+
+        public string LastName { get; set; }
+
+        public Customer(string sSN, string firstName, string lastName)
+        {
+            SSN = sSN;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        private bool IsValidSSN(string sSN)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(sSN, @"^\d{6}$");
+        }
+
+        private bool IsValidName(string firstName)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(firstName, @"^[\p{L}]+$");
+        }
     }
 }
