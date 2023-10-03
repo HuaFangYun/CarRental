a5d7a76c0264b0b82b177f64c024044b07e17cfe
G-C-Code <g-c-code@proton.me>
2023-10-03 10:38:52 +0200

    Solved issue with SeedData not applying validation method correctly (issue lied in missing cast for the generic add method).

--- CarRentalData/Classes/CollectionData.cs
+++ CarRentalData/Classes/CollectionData.cs
@@ -27,24 +27,23 @@ namespace CarRentalData.Classes
         {
             var customers = GetOrCreateList<IPerson>();
             var vehicles = GetOrCreateList<IVehicle>();
-            var bookings = GetOrCreateList<IBooking>();
 
-            customers.Add(new Customer("891010-0168", "Charlie", "Sharp"));
-            customers.Add(new Customer("690830-8572", "Visilia", "Studiya"));
-            customers.Add(new Customer("821003-6612", "Blaine", "Bootstrap"));
+            Add<IPerson>(new Customer("891010-0168", "Charlie", "Sharp"));
+            Add<IPerson>(new Customer("690830-8572", "Visilia", "Studiya"));
+            Add<IPerson>(new Customer("821003-6612", "Blaine", "Bootstrap"));
 
-            vehicles.Add(new Car("AND243", "Porche", 5500, 1f, VehicleType.Touring, 2));
-            vehicles.Add(new Car("AWL853", "Ford", 6000, 1f, VehicleType.Touring, 0));
-            vehicles.Add(new Car("PQZ552", "Duesenberg", 1000, 1.5f, VehicleType.Luxury, 2));
-            vehicles.Add(new Car("VUQ516", "Buick", 5600, 1f, VehicleType.Convertible, 2));
-            vehicles.Add(new Car("IEN716", "Chevrolet", 2950, 1f, VehicleType.Hardtop, 2));
-            vehicles.Add(new Car("ENN712", "Chevrolet", 9700, 1f, VehicleType.Convertible, 2));
-            vehicles.Add(new Motorcycle("MBU852", "Yamaha", 8780, 0.5f, VehicleType.Standard, 1));
-            vehicles.Add(new Motorcycle("WCK661", "Benelli", 7370, 0.5f, VehicleType.Standard, 2));
+            Add<IVehicle>(new Car("AND243", "Porche", 5500, 1f, VehicleType.Touring, 2));
+            Add<IVehicle>(new Car("AWL853", "Ford", 6000, 1f, VehicleType.Touring, 0));
+            Add<IVehicle>(new Car("PQZ552", "Duesenberg", 1000, 1.5f, VehicleType.Luxury, 2));
+            Add<IVehicle>(new Car("VUQ516", "Buick", 5600, 1f, VehicleType.Convertible, 2));
+            Add<IVehicle>(new Car("IEN716", "Chevrolet", 2950, 1f, VehicleType.Hardtop, 2));
+            Add<IVehicle>(new Car("ENN712", "Chevrolet", 9700, 1f, VehicleType.Convertible, 2));
+            Add<IVehicle>(new Motorcycle("MBU852", "Yamaha", 8780, 0.5f, VehicleType.Standard, 1));
+            Add<IVehicle>(new Motorcycle("WCK661", "Benelli", 7370, 0.5f, VehicleType.Standard, 2));
 
-            bookings.Add(new Booking(vehicles[5], customers[0]));
-            bookings.Add(new Booking(vehicles[6], customers[1]));
-            bookings.Add(new Booking(vehicles[7], customers[2]));
+            Add<IBooking>(new Booking(vehicles[5], customers[0]));
+            Add<IBooking>(new Booking(vehicles[6], customers[1]));
+            Add<IBooking>(new Booking(vehicles[7], customers[2]));
         }
 
         private readonly HashSet<string> _checkForDuplicates = new HashSet<string>();
